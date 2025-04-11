573d904c75df04179b23cbe1c78269b0406b7cf8
sanidhya pathak <pathaksanidhya14@gmail.com>
2025-04-11 12:57:31 +0530

    Add files via upload

---
+++ classsports.cpp
@@ -0,0 +1,51 @@
+// write a program for derived class programs  
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class sports {
+public:
+void fit (){
+   cout<< "every sports required fitness "<<endl;
+}
+
+void diet(){
+cout<<"take healthy diet" <<endl;
+}
+
+};
+
+class cricket : public sports{
+public:
+   void boundary(){
+      cout<<"hit the boundaries "<<endl;
+   }
+};
+
+class football : public sports{
+public:
+   void goal(){
+      cout<<"hit the goal  "<<endl;
+   }
+};
+
+
+int main(){
+
+   cricket c;
+   football b;
+
+   cout<<"-------for the sports cricket---------"<<endl;
+   c.fit();
+   c.diet();
+   c.boundary();
+
+
+
+   cout<<"------\nfor the sportfootball-------- "<<endl;
+   b.fit();
+   b.diet();
+   b.goal();
+
+} 
